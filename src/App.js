5db25e6da5ffe07ed46877a0db1d6be75046c97d
ArtemSPN <87538803+ArtemSPN@users.noreply.github.com>
2023-07-12 19:45:54 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,84 @@
-import React, { useState } from 'react';
 import './index.scss';
+import React from 'react';
+
+const questions = [
+    {
+      title: 'React - это ... ?',
+      variants: ['библиотека', 'фреймворк', 'приложение'],
+      correct: 0,
+    },
+    {
+      title: 'Компонент - это ... ',
+      variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+      correct: 1,
+    },
+    {
+      title: 'Что такое JSX?',
+      variants: [
+        'Это простой HTML',
+        'Это функция',
+        'Это тот же HTML, но с возможностью выполнять JS-код',
+      ],
+      correct: 2,
+    },
+  ];
+
+function Result({ restartGame, correct}) {
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
+      <button onClick={restartGame}>Попробовать снова</button>
+    </div>
+  );
+}
+
+function Game({ step, question, onClickVariant }) {
+  const percentage = Math.round((step / questions.length) * 100);
+
+  return (
+    <>
+      <div className="progress">
+        <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+        {question.variants.map((text, index) => (
+          <li onClick={()=> onClickVariant(index)} key={text}>{text}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
 
 function App() {
-    const [isVisible, setIsVisible] = useState(false);
+  const [step, setStep] = React.useState(0);
+  const [correct, setCorrect] = React.useState(0);
+  const question = questions[step];
 
-    const changeVisible = () => {
-        setIsVisible(!isVisible)
+  const onClickVariant = (index) => {
+    setStep(step + 1);
+
+    if(index === question.correct) {
+      setCorrect(correct + 1);
     }
+  }
 
-    return (
-        <div className="App">
-        <button className="open-modal-btn" onClick={changeVisible}>✨ Открыть окно</button>
-        {isVisible && 
-            <div className="overlay" >
-                <div className="modal">
-                <svg height="200" viewBox="0 0 200 200" width="200" onClick={changeVisible}>
-                    <title />
-                    <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-                </svg>
-                <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-                </div>
-             </div>
-        }
-        </div>
+  const restartGame = () => {
+    setStep(0);
+    setCorrect(0);
+  }
+
+  return (
+    <div className="App">
+      {step !== questions.length ? (
+        <Game step={step} question={question} onClickVariant={onClickVariant}/>
+      ) : (
+        <Result restartGame={restartGame} correct={correct} />
+      )
+
+      }
+    </div>
   );
 }
 
