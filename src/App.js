3c5cc17949ae0ac4bd16bbf85b8e974a930e2315
ArtemSPN <87538803+ArtemSPN@users.noreply.github.com>
2023-07-12 19:25:35 +0400

    modal init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './index.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [isVisible, setIsVisible] = useState(false);
 
-  const decrement = () => {
-    setCount(prev => prev - 1)
-  }
+    const changeVisible = () => {
+        setIsVisible(!isVisible)
+    }
 
-  const increment = () => {
-    setCount(prev => prev + 1)
-  }
-
-  return (
-    <div className="App"> 
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>{count}</h1>
-        <button onClick={decrement} className="minus">- Минус</button>
-        <button onClick={increment} className="plus">Плюс +</button>
-      </div>
-    </div>
+    return (
+        <div className="App">
+        <button className="open-modal-btn" onClick={changeVisible}>✨ Открыть окно</button>
+        {isVisible && 
+            <div className="overlay" >
+                <div className="modal">
+                <svg height="200" viewBox="0 0 200 200" width="200" onClick={changeVisible}>
+                    <title />
+                    <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+                </svg>
+                <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+                </div>
+             </div>
+        }
+        </div>
   );
 }
 
